ea7b2ceeb7d3bfcf8e7afaaf1429c39098ecb7f4
cjsalame <cjsalame@uc.cl>
2015-10-09 00:40:14 -0300

    usuario puede crear pedidos y encomiendas

--- src/chilexplox/Usuario.java
+++ src/chilexplox/Usuario.java
@@ -18,4 +18,23 @@ public class Usuario {
         this.nombreUsuario = nombreUsuario;
         this.contrasena = contrasena;
     }
+    
+    public Pedido crearPedido(Sucursal sucOrigen, Sucursal sucDestino, Cliente cliente){
+        Pedido p = new Pedido(sucOrigen, sucDestino, cliente);
+        return p;
+    }
+    
+    public void agregarEnc(Pedido p, int peso, int volumen, Sucursal sucOrigen,Sucursal sucDestino,
+            String dirDestino){
+        p.agregarEnc(peso, volumen, sucOrigen, sucDestino, dirDestino);
+    }
+    
+    public void agregarEnc(Pedido p,int peso, int volumen, int prioridad, Sucursal sucOrigen,
+            Sucursal sucDestino, String dirDestino){
+        p.agregarEnc(peso, volumen, prioridad, sucOrigen, sucDestino, dirDestino);
+    }
+    
+    
+    
+    
 }

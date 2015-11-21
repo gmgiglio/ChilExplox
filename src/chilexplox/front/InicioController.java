59fdaee9c6be3a8dfaa0e701f12afd08e59538ac
Alberto León Salinas <ajleon@uc.cl>
2015-11-21 20:03:29 -0300

    Cambios en aceptar Edición encomienda e ícono de error

--- src/chilexplox/front/InicioController.java
+++ src/chilexplox/front/InicioController.java
@@ -9,6 +9,7 @@ import controllers.EditarEncomiendaController;
 import controllers.FichaCliente;
 import java.net.URL;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -640,12 +641,35 @@ public class InicioController implements Initializable {
              CajaEncomienda caja = (CajaEncomienda) e.getSource();
              Encomienda enc = caja.getEncomienda();
              editarEncomiendaCon = new EditarEncomiendaController(enc);
+             editarEncomiendaCon.setHandlerAceptarEncomienda((Event ev) -> {
+                 eliminarEncomienda(editarEncomiendaCon.getEnc());
+                 agregarEncomienda(editarEncomiendaCon.getEncomiendaMod());
+            });
                                 agregarPane.getChildren().setAll(editarEncomiendaCon);
                                 split.setDividerPositions(0.4684014869888476);
          });
          listEncomiendas.getItems().add(c);
          //comboBoxEncomiendas.setPromptText(encomienda.getDescripcion());
          presupuesto.setText(""+((Funcionario)Main.getUsuarioActual()).getSucActual().getPedidoAbierto().getCostoEnvio());
+     }
+     
+     private void eliminarEncomienda(Encomienda encomienda){
+         ((Funcionario)Main.getUsuarioActual()).getSucActual().getPedidoAbierto().eliminarEncomienda(encomienda);
+         List<CajaEncomienda> listcajas = listEncomiendas.getItems(); 
+         
+         for(CajaEncomienda c:listcajas)
+         {
+             if(c.getEncomienda()==encomienda){
+              
+                 listEncomiendas.getItems().remove(c);
+                 break;
+             }
+         
+         }
+        
+        presupuesto.setText(""+((Funcionario)Main.getUsuarioActual()).getSucActual().getPedidoAbierto().getCostoEnvio());
+
+      
      }
             
      private class ItemSucursalMenu extends MenuItem{

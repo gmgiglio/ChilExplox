1394cfe9bd627838657919dee8f5b1dc6fc574bf
Alberto León Salinas <ajleon@uc.cl>
2015-11-22 02:01:17 -0300

    Buzon de entrada, enviados e íconos

--- src/controllers/CajaMensajeEnviado.java
+++ src/controllers/CajaMensajeEnviado.java
@@ -8,9 +8,12 @@ package controllers;
 import chilexplox.Mensaje;
 import chilexplox.RegistroMensaje;
 import java.io.IOException;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 /**
@@ -27,6 +30,10 @@ public class CajaMensajeEnviado extends Button {
     private RegistroMensaje registro;
     
     private int largoMaxAsunto = 40;
+    
+    private static final String STANDARD_BUTTON_STYLE = "-fx-background-color: #FFF;";
+    private static final String HOVERED_BUTTON_STYLE  = "-fx-background-color: #F1F1F1;";
+    private static final String ACTIVE_BUTTON_STYLE  = "-fx-background-color: #1269D9;";
     
     public CajaMensajeEnviado(RegistroMensaje registro){
         this.registro = registro;
@@ -54,6 +61,36 @@ public class CajaMensajeEnviado extends Button {
             textoAsunto.setText(s.substring(0, largoMaxAsunto) + "...");
         }
         else textoAsunto.setText(s);
+    }
+     
+      public void changeBackgroundOnHover(){
+    setStyle(STANDARD_BUTTON_STYLE);
+    setOnMouseEntered(new EventHandler<MouseEvent>() {
+      @Override public void handle(MouseEvent mouseEvent) {
+        setStyle(HOVERED_BUTTON_STYLE);
+        textoAsunto.setFill(Color.BLACK);
+        textoSucursal.setFill(Color.BLACK);  
+      }
+    });
+    
+    setOnMouseExited(new EventHandler<MouseEvent>() {
+      @Override public void handle(MouseEvent mouseEvent) {
+        setStyle(STANDARD_BUTTON_STYLE);
+        textoAsunto.setFill(Color.BLACK);
+        textoSucursal.setFill(Color.BLACK);
+
+      }
+    });
+    this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+           @Override public void handle(MouseEvent mouseEvent) {
+                
+                setStyle(ACTIVE_BUTTON_STYLE);
+                textoAsunto.setFill(Color.WHITE);
+                textoSucursal.setFill(Color.WHITE);
+            }
+    }); 
+   
+        
     }
     
 }
